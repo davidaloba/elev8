d83e6de5ad9caaab2097caaf517d0f76fa2a9b94
mistertaavetti <alobalolu@gmail.com>
2022-02-23 00:54:38 +0100

    init-tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
